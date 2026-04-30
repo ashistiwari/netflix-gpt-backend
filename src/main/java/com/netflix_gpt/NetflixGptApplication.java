5ba10b1c0c627ee5a1e5761d52ca3074d76c564a
ashistiwari <ashistiwari39@gmail.com>
2026-04-30 12:02:18 +0530

    "Added loggers for url and username"

--- src/main/java/com/netflix_gpt/NetflixGptApplication.java
+++ src/main/java/com/netflix_gpt/NetflixGptApplication.java
@@ -8,6 +8,9 @@ public class NetflixGptApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixGptApplication.class, args);
+		System.out.println("URL=" + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("USER=" + System.getenv("SPRING_DATASOURCE_USERNAME"));
+
 	}
 
 }
