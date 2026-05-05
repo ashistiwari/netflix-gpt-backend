1a38bb405532fcc5fc66ce0b35533f93fc1898e8
ashistiwari39 <ashistiwari39@gmail.com>
2026-05-05 19:26:59 +0530

    git issue checking

--- src/main/java/com/netflix_gpt/config/SecurityConfig.java
+++ src/main/java/com/netflix_gpt/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println("checking commit issue");
 
         http
                 .csrf(csrf -> csrf.disable())
